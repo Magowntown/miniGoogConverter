2db3d22f32cec6a645967747dca78a26fc53f9ec
Magowntown <amagown@gmail.com>
2023-06-20 11:00:54 -0400

    switch case example

--- js/app.js
+++ js/app.js
@@ -1,88 +1,93 @@
-window.onload = function () {
-  document.getElementById("clear").addEventListener("click", clearValues);
-  document
-    .getElementById("measurement")
-    .addEventListener("change", updateSelectOptions);
-  document.getElementById("inputBox").addEventListener("input", convertValue);
-  updateSelectOptions();
-};
+let measurementOptions = document.querySelector("#measurementOptions");
+let length = document.querySelector("#length");
+let fuel = document.querySelector("#fuel");
+let storage = document.querySelector("#storage");
 
-function updateSelectOptions() {
-  const measurement = document.getElementById("measurement").value.trim();
-  let selectElementId = "";
-  let options = [];
+let input = document.querySelector("#input");
+let result = document.createElement("#result");
 
-  switch (measurement) {
+let inputType = document.createElement("#inputType");
+let resultType = document.createElement("#resultType");
+
+let clear = document.querySelector("#clear");
+
+// Event Listeners
+
+function createOptions(measurementOption) {
+  // Clear the existing options from the inputType and resultType select elements
+  document.getElementById("inputType").innerHTML = "";
+  document.getElementById("resultType").innerHTML = "";
+
+  // Add the appropriate options to the inputType and resultType select elements depending on the measurementOption
+  switch (measurementOption) {
     case "length":
-      selectElementId = "distanceUnit";
-      options = [
-        { value: "km", text: "Kilometer" },
-        { value: "miles", text: "Miles" },
-      ];
+      document.getElementById("inputType").innerHTML += `
+        <option value="km">km</option>
+        <option value="miles">miles</option>
+      `;
+      document.getElementById("resultType").innerHTML += `
+        <option value="km">km</option>
+        <option value="miles">miles</option>
+      `;
       break;
-    case "fuel":
-      selectElementId = "gasUnit";
-      options = [
-        { value: "kmpl", text: "Kilometer per liter" },
-        { value: "mpg", text: "Miles per gallon" },
-      ];
+    case "fuel economy":
+      document.getElementById("inputType").innerHTML += `
+        <option value="kpl">kpl</option>
+        <option value="mpg">mpg</option>
+      `;
+      document.getElementById("resultType").innerHTML += `
+        <option value="kpl">kpl</option>
+        <option value="mpg">mpg</option>
+      `;
       break;
-    case "storage":
-      selectElementId = "storageUnit";
-      options = [
-        { value: "byte", text: "Byte" },
-        { value: "kb", text: "Kilobyte" },
-      ];
+    case "digital storage":
+      document.getElementById("inputType").innerHTML += `
+        <option value="bytes">bytes</option>
+        <option value="kilobytes">KB</option>
+      `;
+      document.getElementById("resultType").innerHTML += `
+        <option value="bytes">bytes</option>
+        <option value="kilobytes">KB</option>
+      `;
       break;
   }
-
-  let selectElement = document.getElementById(selectElementId);
-  selectElement.innerHTML = "";
-  options.forEach((option) => {
-    let optionElement = document.createElement("option");
-    optionElement.value = option.value;
-    optionElement.text = option.text;
-    selectElement.appendChild(optionElement);
-  });
-  selectElement.addEventListener("change", convertValue);
 }
 
-function convertValue() {
-  let inputValue = document.getElementById("inputBox").value;
-  let measurement = document.getElementById("measurement").value.trim();
-  let unitElementId =
-    measurement === "length"
-      ? "distanceUnit"
-      : measurement === "fuel"
-      ? "gasUnit"
-      : "storageUnit";
-  let unit = document.getElementById(unitElementId).value;
-  let convertedValue = 0;
+// When the measurementOption select element changes, call the createOptions() function
+document
+  .getElementById("measurementOptions")
+  .addEventListener("change", createOptions);
 
-  if (measurement === "length") {
-    if (unit === "km") {
-      convertedValue = inputValue / 1.60934;
-    } else {
-      convertedValue = inputValue * 1.60934;
-    }
-  } else if (measurement === "fuel") {
-    if (unit === "kmpl") {
-      convertedValue = inputValue * 2.35214;
-    } else {
-      convertedValue = inputValue / 2.35214;
-    }
-  } else {
-    if (unit === "byte") {
-      convertedValue = inputValue / 1024;
-    } else {
-      convertedValue = inputValue * 1024;
-    }
-  }
+// Allow for the following conversions:
 
-  document.getElementById("output").value = convertedValue;
+// Length: km -> miles
+// Fuel Economy: Kilometer per liter -> Miles per gallon
+// Digital Storage: Byte -> Kilobyte
+// Use functions to “group” your formulas
+
+// 1. Length Conversion
+function kmToMiles(km) {
+  return km * 0.621371;
 }
 
-function clearValues() {
-  document.getElementById("inputBox").value = "";
-  document.getElementById("output").value = "";
+function milesToKm(miles) {
+  return miles * 1.60934;
+}
+
+// 2. Fuel Economy Conversion
+function kilToKm(kil) {
+  return kil * 2.35215;
+}
+
+function kmToKil(km) {
+  return km * 0.425144;
+}
+
+// 3. Digital Storage Conversion
+function byteToKb(byte) {
+  return byte * 0.000976563;
+}
+
+function kilToByte(kil) {
+  return kil * 1024;
 }
